a5c39293c43cd3a7ac49eac72d03d914301a1687
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2025-06-02 18:42:06 +0700

    add class pewarisan dan public

--- ParadigmaOOP/noPewarisan.cpp
+++ ParadigmaOOP/noPewarisan.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+
+class baseclass final
+{
+public :
+    virtual void perkenalan()
+    {
+        cout << "hallo saya function dari base class";
+    }
+};
