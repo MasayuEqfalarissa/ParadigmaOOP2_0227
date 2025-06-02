aebf55d30fd8ac1e5b2ff831c23b9fda3000d4c2
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2025-06-02 19:04:51 +0700

    add class baseclass dan public virtual void

--- ParadigmaOOP/no0verriding.cpp
+++ ParadigmaOOP/no0verriding.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 using namespace std;
 
+class baseclass 
+{
+public :
+    virtual void perkenalan() final
+    {
+        cout << "hallo saya function dari base class";
+    }
+};
+
